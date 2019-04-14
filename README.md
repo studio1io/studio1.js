943065241b74f3b1f3f62b3cc68ab02c26e282aa
Sebastian Seilund <sebsei@gmail.com>
2019-04-14 15:38:21 -0700

    Ensure that window.studio1 exists before resolving

--- README.md
+++ README.md
@@ -1,3 +1,5 @@
 # studio1.js
 
+Async loader for studio1.js.
+
 New product in semi-stealth. Docs coming soon!
--- index.js
+++ index.js
@@ -6,7 +6,17 @@ module.exports.loadStudio1 = function(opts) {
     loadStudio1Promise = new Promise((resolve, reject) => {
       const script = document.createElement('script')
       script.src = opts.url || 'https://js.studio1.io/studio1.js'
-      script.onload = () => resolve(window['studio1'])
+      script.onload = () => {
+        if (!window.studio1) {
+          reject(
+            new Error(
+              'Studio1.js script was loaded, but the studio1 global was not found.'
+            )
+          )
+          return
+        }
+        resolve(window.studio1)
+      }
       script.onerror = () => {
         loadStudio1Promise = undefined
         reject(new Error('Could not load Studio1.js'))
